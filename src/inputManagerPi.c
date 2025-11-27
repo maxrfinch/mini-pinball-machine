ec4d0e334273f6e298db8d2270a19a28a3ffa421
Max Finch <45885290+maxrfinch@users.noreply.github.com>
2025-11-26 22:40:45 -0800

    Update inputManagerPi.c

--- src/inputManagerPi.c
+++ src/inputManagerPi.c
@@ -179,7 +179,7 @@ InputManager* inputInit(){
     receiveBufferPos = 0;
     
     // Set NeoPixel brightness to 80 out of 255 at startup
-    sprintf(tempString, "CMD NEO BRIGHTNESS 80\n");
+    sprintf(tempString, "CMD NEO BRIGHTNESS 65\n");
     sendCommand(input->fd, tempString);
     
     return input;
